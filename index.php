629f2f26463a7bf3967a0b24c4b50300aeb7cb5e
Daniil Gentili <daniil.gentili.dg@gmail.com>
2016-04-19 23:08:12 +0200

    Finished getfile proxy method. Now to testing....

--- index.php
+++ index.php
@@ -36,71 +36,81 @@ function curl($url) {
 	return json_decode($res, true);
 };
 
+function checkurl($url) {
+	$ch = curl_init("$url");
+	curl_setopt($ch, CURLOPT_NOBODY, true);
+	curl_exec($ch);
+	$retcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+	curl_close($ch);
+	if($retcode = 200) { return 0 else return 1;
+}
+
 // Prepare the url with the token
-$url = "https://telegram.org/" . preg_replace(array("/^\//", "/\/.*/"), '', $_SERVER['REQUEST_URI']);
+$token = preg_replace(array("/^\//", "/\/.*/"), '', $_SERVER['REQUEST_URI']);
+$url = "https://telegram.org/" . $token;
 
 
 // gotta intercept sendphoto, sendaudio, sendvoice, sendvideo, senddocument, possibly without storing in ram, upload as secret user, forward to user
 
 
 // intercept getfile, get file id, forward to secret user, get download link from secret user, return file id, file size, file path
-if($_SERVER['REQUEST_URI'] == "/getFile" && $_REQUEST['file_id'] != "") {
- $response = curl($url . "/getFile?file_id=" . $_REQUEST['file_id']);
- if($response["ok"] == false && preg_match("/\[Error\]: Bad Request: file is too big\[size:/", $response["description"])) {
- 
-  $size = preg_replace(array("/.*big\[size:/", "/\].*/"), '', $response["description"]); // get size
+if(strtolower($_SERVER['REQUEST_URI']) == "/getFile" && $_REQUEST['file_id'] != "") {
+	$response = curl($url . "/getFile?file_id=" . $_REQUEST['file_id']);
+	if($response["ok"] == false && preg_match("/\[Error\]: Bad Request: file is too big\[size:/", $response["description"])) {
+		$databasestmt = $pdo->prepare("SELECT * FROM dl WHERE file_id=? DESC LIMIT 1;");
+		$database = $databasestmt->execute(array($_REQUEST['file_id']));
+		if(checkurl("https://storage.n2ogram.daniil.it/" . hash('sha256', $token) . $database["file_path"])) {
+			$newresponse = $database;
+			$newresponse["ok"] = true;
+		} else {
+			$size = preg_replace(array("/.*big\[size:/", "/\].*/"), '', $response["description"]); // get size
+			$me = curl($url . "/getMe")["username"]; // get my username
+			$usernames = array_column($telegram->getDialogList(), "username"); // get usernames list
+			if(!in_array($me, $usernames)) { // If never contacted bot send start command
+				$telegram->msg($me, '/start');
+			}
 
-  
-  $me = curl($url . "/getMe")["username"]; // get my username
-  
-  $usernames = array_column($telegram->getDialogList(), "username"); // get usernames list
-  
-  if(!in_array($me, $usernames)) { // If never contacted bot send start command
-   $telegram->msg($me, '/start');
-  }
+			$count = 0;
+			while($result["ok"] != true) {
+				$result = curl($url . "/send" . $sendMethods["$count"] . "?chat_id=" . $botusername . "&" . $sendMethods["$count"] . "=" . $_REQUEST['file_id']);
+				$count++;
+			};
 
-  $count = 0;
-  while($result["ok"] != true) {
-   $result = curl($url . "/send" . $sendMethods["$count"] . "?chat_id=" . $botusername . "&" . $sendMethods["$count"] . "=" . $_REQUEST['file_id']);
-   $count++;
-  };
+			if($result["ok"] == false) {
+				$newresponse["ok"] = false;
+				$newresponse["error_code"] = 400;
+				$newresponse["description"] = "Couldn't forward file to download user.";
+			} else {
+				$result = curl($url . "/sendMessage?reply_to_message_id=" . $result["message_id"] . "&chat_id=" . $botusername . "&message=" . $_REQUEST['file_id']);
+				if($result["ok"] == false) {
+					$newresponse["ok"] = false;
+					$newresponse["error_code"] = 400;
+					$newresponse["description"] = "Couldn't reply to forwarded file to download user.";
+				} else {
+					$path = $telegram->getFile($me, $sendMethods["$count"], find_txt($telegram->getHistory($botusername, 1000000, 1000000)))["result"];
+					if($path = "") {
+						$newresponse["ok"] = false;
+						$newresponse["error_code"] = 400;
+						$newresponse["description"] = "Couldn't download file.";
+					} else {
+						if(filesize($path) != $size) {
+							$newresponse["ok"] = false;
+							$newresponse["error_code"] = 400;
+							$newresponse["description"] = "Downloaded file size does not match.";
+						} else {
+							$newresponse["ok"] = true;
+							$newresponse["file_id"] = $_REQUEST['file_id'];
+							$newresponse["file_size"] = $size;
+							$newresponse["file_path"] = preg_replace('/\/mnt\/vdb\/api\/files\//', '', $path);
+						}
+					}
+				}
+			}
+		}
 
-  if($result["ok"] == false) {
-   $newresponse["ok"] = false;
-   $newresponse["error_code"] = 400;
-   $newresponse["description"] = "Couldn't forward file to download user.";
-  } else {
-   $result = curl($url . "/sendMessage?reply_to_message_id=" . $result["message_id"] . "&chat_id=" . $botusername . "&message=" . $_REQUEST['file_id']);
-   if($result["ok"] == false) {
-    $newresponse["ok"] = false;
-    $newresponse["error_code"] = 400;
-    $newresponse["description"] = "Couldn't reply to forwarded file to download user.";
-   } else {
-    $path = $telegram->getFile($me, $sendMethods["$count"], find_txt($telegram->getHistory($botusername, 1000000, 1000000)))["result"];
-    if($path = "") {
-     $newresponse["ok"] = false;
-     $newresponse["error_code"] = 400;
-     $newresponse["description"] = "Couldn't download file.";
-    } else {
-     if(filesize($path) != $size)
-      $newresponse["ok"] = false;
-      $newresponse["error_code"] = 400;
-      $newresponse["description"] = "Downloaded file size does not match.";
-      } else {
-       $newresponse["ok"] = true;
-       $newresponse["file_id"] = $_REQUEST['file_id'];
-       $newresponse["file_size"] = $size;
-       $newresponse["file_path"] = preg_replace('/\/mnt\/vdb\/api\/files\//', '', $path);
-      }
-     }
-    }
-   }
-  }
-
-  
-  $response = json_encode($newresponse);
- }
- exit($response);
+		$response = json_encode($newresponse);
+	}
+	exit($response);
 }
 include "proxy.php";
 ?>
