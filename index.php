6b05ba92a5adc3c2d2aaf7f94cb77551ec36da4f
Daniil Gentili <daniil.gentili.dg@gmail.com>
2016-04-20 16:28:48 +0200

    Almost there...

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-// n2ogram script
+// pwrtelegram script
 // by Daniil Gentili
 // gplv3 license
 
@@ -14,14 +14,12 @@ $sendMethods = array("document", "photo", "audio", "voice", "video");
 
 
 function find_txt($msgs) {
-   foreach ($msgs as $msg) {
-     foreach ($msg as $key => $val) { 
-      if ($key = "text" && $val = $_REQUEST['file_id']) {
-        $ok = y;
-      }
-     }
-     if ($ok = "y") return $msg["reply_id"];
-    }
+	foreach ($msgs as $msg) {
+		foreach ($msg as $key => $val) { 
+			if ($key = "text" && $val = $_REQUEST["file_id"]) $ok = y;
+		}
+		if ($ok = "y") return $msg["reply_id"];
+	}
 }
 // curl wrapper
 function curl($url) {
@@ -42,7 +40,7 @@ function checkurl($url) {
 	curl_exec($ch);
 	$retcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 	curl_close($ch);
-	if($retcode = 200) { return 0 else return 1;
+	if($retcode = 200) { return 0; } else { return 1;  };
 }
 
 // Prepare the url with the token
@@ -54,17 +52,22 @@ $url = "https://telegram.org/" . $token;
 
 
 // intercept getfile, get file id, forward to secret user, get download link from secret user, return file id, file size, file path
+
 if(strtolower($_SERVER['REQUEST_URI']) == "/getFile" && $_REQUEST['file_id'] != "") {
 	$response = curl($url . "/getFile?file_id=" . $_REQUEST['file_id']);
 	if($response["ok"] == false && preg_match("/\[Error\]: Bad Request: file is too big\[size:/", $response["description"])) {
-		$databasestmt = $pdo->prepare("SELECT * FROM dl WHERE file_id=? DESC LIMIT 1;");
-		$database = $databasestmt->execute(array($_REQUEST['file_id']));
-		if(checkurl("https://storage.n2ogram.daniil.it/" . hash('sha256', $token) . $database["file_path"])) {
-			$newresponse = $database;
+		$file_id = $_REQUEST['file_id'];
+		$selectstmt = $pdo->prepare("SELECT * FROM dl WHERE file_id=? DESC LIMIT 1;");
+		$select = $selectstmt->execute(array($file_id));
+		if(checkurl("https://storage.pwrtelegram.xyz/" . $select["file_path"])) {
+			$newresponse["file_id"] = $select["file_id"];
+			$newresponse["file_path"] = $select["file_path"];
+			$newresponse["file_size"] = $select["file_size"];
 			$newresponse["ok"] = true;
 		} else {
-			$size = preg_replace(array("/.*big\[size:/", "/\].*/"), '', $response["description"]); // get size
+			$file_size = preg_replace(array("/.*big\[size:/", "/\].*/"), '', $response["description"]); // get size
 			$me = curl($url . "/getMe")["username"]; // get my username
+
 			$usernames = array_column($telegram->getDialogList(), "username"); // get usernames list
 			if(!in_array($me, $usernames)) { // If never contacted bot send start command
 				$telegram->msg($me, '/start');
@@ -72,7 +75,7 @@ if(strtolower($_SERVER['REQUEST_URI']) == "/getFile" && $_REQUEST['file_id'] != 
 
 			$count = 0;
 			while($result["ok"] != true) {
-				$result = curl($url . "/send" . $sendMethods["$count"] . "?chat_id=" . $botusername . "&" . $sendMethods["$count"] . "=" . $_REQUEST['file_id']);
+				$result = curl($url . "/send" . $sendMethods["$count"] . "?chat_id=" . $botusername . "&" . $sendMethods["$count"] . "=" . $file_id);
 				$count++;
 			};
 
@@ -81,7 +84,7 @@ if(strtolower($_SERVER['REQUEST_URI']) == "/getFile" && $_REQUEST['file_id'] != 
 				$newresponse["error_code"] = 400;
 				$newresponse["description"] = "Couldn't forward file to download user.";
 			} else {
-				$result = curl($url . "/sendMessage?reply_to_message_id=" . $result["message_id"] . "&chat_id=" . $botusername . "&message=" . $_REQUEST['file_id']);
+				$result = curl($url . "/sendMessage?reply_to_message_id=" . $result["message_id"] . "&chat_id=" . $botusername . "&message=" . $file_id);
 				if($result["ok"] == false) {
 					$newresponse["ok"] = false;
 					$newresponse["error_code"] = 400;
@@ -93,15 +96,23 @@ if(strtolower($_SERVER['REQUEST_URI']) == "/getFile" && $_REQUEST['file_id'] != 
 						$newresponse["error_code"] = 400;
 						$newresponse["description"] = "Couldn't download file.";
 					} else {
-						if(filesize($path) != $size) {
+						if(filesize($path) != $file_size) {
 							$newresponse["ok"] = false;
 							$newresponse["error_code"] = 400;
 							$newresponse["description"] = "Downloaded file size does not match.";
+							unlink($path);
 						} else {
-							$newresponse["ok"] = true;
-							$newresponse["file_id"] = $_REQUEST['file_id'];
-							$newresponse["file_size"] = $size;
-							$newresponse["file_path"] = preg_replace('/\/mnt\/vdb\/api\/files\//', '', $path);
+							$file_path = hash('sha256', $token) . preg_replace('/\/mnt\/vdb\/api\/files/', '', $path);
+							if(rename($path, "/mnt/vdb/api/storage/" . $file_path)) {
+								$newresponse["ok"] = true;
+								$newresponse["file_id"] = $file_id;
+								$newresponse["file_size"] = $file_size;
+								$newresponse["file_path"] = $file_path;
+								$delete_stmt = $pdo->prepare("DELETE FROM dl WHERE file_id=?;");
+								$delete = $delete_stmt->execute(array($file_id));
+								$insert_stmt = $pdo->prepare("INSERT INTO dl VALUES (file_id, file_size, file_path), (?, ?, ?);");
+								$insert = $insert_stmt->execute(array($file_id, $file_size, $file_path));
+							}
 						}
 					}
 				}
