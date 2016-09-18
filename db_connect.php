41eb9205aa4fbf0d31f919cb66ccfec875ac0db8
Daniil Gentili <daniil.gentili.dg@gmail.com>
2016-09-18 20:28:39 +0200

    Readded db_connect.php

---
+++ db_connect.php
@@ -0,0 +1,4 @@
+<?php
+require_once '../db_connect.php';
+$pdo = new PDO($deep ? $deepdb : $db, $deep ? $deepdbuser : $dbuser, $deep ? $deepdbpassword : $dbpassword);
+$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
