22202916815023e21b248352a8664befa1b4f8d3
Daniil Gentili <daniil.gentili.dg@gmail.com>
2016-09-18 18:28:53 +0000

    Applied fixes from StyleCI

--- db_connect.php
+++ db_connect.php
@@ -1,4 +1,5 @@
 <?php
+
 require_once '../db_connect.php';
 $pdo = new PDO($deep ? $deepdb : $db, $deep ? $deepdbuser : $dbuser, $deep ? $deepdbpassword : $dbpassword);
 $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
