e3531e97f03770303293e291815b08223798e69d
Daniil Gentili <daniil.gentili.dg@gmail.com>
2016-11-11 15:34:21 +0000

    Removed save file

--- db_connect.php
+++ db_connect.php
@@ -3,3 +3,4 @@
 require_once '../db_connect.php';
 $pdo = new PDO($deep ? $deepdb : $db, $deep ? $deepdbuser : $dbuser, $deep ? $deepdbpassword : $dbpassword);
 $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
