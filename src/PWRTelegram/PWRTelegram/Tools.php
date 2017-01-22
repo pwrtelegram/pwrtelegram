d95f0fb264af3cdc03a0d8e4f5ef1e591f6b8198
Daniil Gentili <daniil.gentili.dg@gmail.com>
2017-01-22 16:02:09 +0100

    Bugfix

--- src/PWRTelegram/PWRTelegram/Tools.php
+++ src/PWRTelegram/PWRTelegram/Tools.php
@@ -176,8 +176,8 @@ class Tools
             if ($this->curl($this->url.'/sendMessage?text=SHISH&chat_id='.$this->botusername)['ok']) {
                 return $this->userchecked = true;
             }
-            $this->madeline_connect();
-            $this->madeline->messages->sendMessage(['peer' => '@'.$me, 'message' => '/start']);
+            $this->madeline_backend_connect();
+            $this->madeline_backend->messages->sendMessage(['peer' => '@'.$me, 'message' => '/start']);
             if ($this->curl($this->url.'/sendMessage?text=SHISH&chat_id='.$this->botusername)['ok']) {
                 return $this->userchecked = true;
             }
