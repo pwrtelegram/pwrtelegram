33ddb66ecde7912eb13cc9e90b0867fdf9ca70ee
Daniil Gentili <daniil.gentili.dg@gmail.com>
2017-02-04 01:23:04 +0100

    :(

--- src/PWRTelegram/PWRTelegram/Main.php
+++ src/PWRTelegram/PWRTelegram/Main.php
@@ -28,7 +28,7 @@ class Main extends Proxy
             $this->madeline->API->reset_session();
             \danog\MadelineProto\Serialization::serialize($this->madeline_path, $this->madeline);
         }
-        if (isset($this->madeline_backend) && filemtime($this->madeline_backend_path) < time() - 1)) {
+        if (isset($this->madeline_backend) && filemtime($this->madeline_backend_path) < time() - 1) {
             $this->madeline_backend->API->reset_session();
             \danog\MadelineProto\Serialization::serialize($this->madeline_backend_path, $this->madeline_backend);
         }
