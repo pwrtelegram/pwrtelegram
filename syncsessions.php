34e8cdf6f4f9cf17c23bad4dd6105dcf1f0635bc
Daniil Gentili <daniil.gentili.dg@gmail.com>
2018-03-09 19:55:51 +0000

    Apply fixes from StyleCI

--- syncsessions.php
+++ syncsessions.php
@@ -1,4 +1,5 @@
 <?php
+
 require 'vendor/autoload.php';
 require '../storage_url.php';
 
@@ -7,20 +8,25 @@ ini_set('error_log', '/tmp/maintainance.log');
 
 $pids = [];
 foreach (glob('../sessions/*.madeline') as $session) {
-    echo "Creating fork (".count($pids)." processes running)\n";
+    echo 'Creating fork ('.count($pids)." processes running)\n";
     if (($pid = pcntl_fork()) === 0) {
         try {
             $madeline = new \danog\MadelineProto\API($session, ['logger' => ['logger_level' => 5], 'connection_settings' => ['all' => ['protocol' => 'tcp_abridged']], 'peer' => ['cache_all_peers_on_startup' => true, 'full_fetch' => true], 'pwr' => ['db_token' => $db_token, 'pwr' => true, 'strict' => true]]);
-            if (!isset($madeline->settings['pwr']['update_handler'])) $madeline->API->updates = [];
+            if (!isset($madeline->settings['pwr']['update_handler'])) {
+                $madeline->API->updates = [];
+            }
             foreach ($madeline->API->chats as $chat) {
                 try {
                     $madeline->get_pwr_chat($chat);
                 } catch (\danog\MadelineProto\RPCErrorException $e) {
-                    if (in_array($e->rpc, ['CHANNEL_PRIVATE', 'CHAT_FORBIDDEN'])) continue;
+                    if (in_array($e->rpc, ['CHANNEL_PRIVATE', 'CHAT_FORBIDDEN'])) {
+                        continue;
+                    }
                     if (preg_match('/FLOOD_WAIT_(\d*)/', $e->rpc, $matches)) {
                         sleep($matches[1]);
                         continue;
                     }
+
                     throw $e;
                 }
             }
@@ -29,7 +35,9 @@ foreach (glob('../sessions/*.madeline') as $session) {
         } catch (\danog\MadelineProto\RPCErrorException $e) {
             if (in_array($e->rpc, ['SESSION_REVOKED', 'AUTH_KEY_UNREGISTERED', 'USER_DEACTIVATED'])) {
                 echo "Deleting session $session after ".$e->rpc.'...'.PHP_EOL;
-                if (isset($madeline)) $madeline->session = '';
+                if (isset($madeline)) {
+                    $madeline->session = '';
+                }
                 foreach (glob($session.'*') as $path) {
                     unlink($path);
                 }
@@ -41,7 +49,7 @@ foreach (glob('../sessions/*.madeline') as $session) {
         }
         die;
     }
-    $pids []= $pid;
+    $pids[] = $pid;
     while (count($pids) > 20) {
         echo "Waiting for CPU time...\n";
         foreach ($pids as $key => $pid) {
